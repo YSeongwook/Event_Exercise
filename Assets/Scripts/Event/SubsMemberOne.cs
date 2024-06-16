18a32e96d22bee21f52513747465b3e7bc6cd98d
danielcho <hellena1717@gmail.com>
2024-06-16 22:48:20 +0900

    [feat] 구독자 - 이벤트 발생자 연동 및 구독/구독해제 추가
    - 구독자는 이벤트 발생자가 있거나 없거나 알아서 구독하고 싶으면 구독

--- Assets/Scripts/Event/SubsMemberOne.cs
+++ Assets/Scripts/Event/SubsMemberOne.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public class SubsMemberOne : MonoBehaviour
 {
+    [SerializeField] EventMaker EventMaker_Publisher;
     [SerializeField] Animator Animator_SubMember;
+
+    private void OnEnable()
+    {
+        EventMaker_Publisher.Subscribe(isSubscribe:true, OnEventMakerInvoked);
+    }
+
+    private void OnDisable()
+    {
+        EventMaker_Publisher.Subscribe(isSubscribe:false, OnEventMakerInvoked);
+    }
 
     public void OnEventMakerInvoked()
     {
--- Assets/Scripts/Event/SubsMemberTwo.cs
+++ Assets/Scripts/Event/SubsMemberTwo.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class SubsMemberTwo : MonoBehaviour
 {
+    [SerializeField] EventMaker EventMaker_Publisher;
     [SerializeField] Animator Animator_SubMember;
 
+
+    private void OnEnable()
+    {
+        EventMaker_Publisher.Subscribe(isSubscribe: true, OnEventLalala);
+    }
+
+    private void OnDisable()
+    {
+        EventMaker_Publisher.Subscribe(isSubscribe: false, OnEventLalala);
+    }
     public void OnEventLalala()
     {
         Animator_SubMember.SetTrigger("LevelUp");
